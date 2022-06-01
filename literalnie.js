dda9b141fc25b6c69cedb405f2f3e87f4f9a06c9
Mikołaj <kaleta222@gmail.com>
2022-06-01 12:08:03 +0200

    Question modal added fully

--- literalnie.js
+++ literalnie.js
@@ -5,8 +5,40 @@ const napis = document.querySelector('#napis');
 const inp = document.querySelectorAll('.inp');
 //const reset = document.querySelector('#reset');
 
+var modal = document.getElementById('id01');
 
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
 
+function openTab(cityName, elmnt, color) {
+	// Hide all elements with class="tabcontent" by default */
+	var i, tabcontent, tablinks;
+	tabcontent = document.getElementsByClassName("tabcontent");
+	for (i = 0; i < tabcontent.length; i++) {
+	  tabcontent[i].style.display = "none";
+	}
+  
+	// Remove the background color of all tablinks/buttons
+	tablinks = document.getElementsByClassName("tablink");
+	for (i = 0; i < tablinks.length; i++) {
+	  tablinks[i].style.backgroundColor = "";
+	}
+  
+	// Show the specific tab content
+	document.getElementById(cityName).style.display = "block";
+  
+	// Add the specific color to the button used to open the tab content
+	elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+  
 function yellow() {
 	if (!check.checked) napis.innerHTML = 'Kliknij raz na literę jeśli szukane słowo jej <u>nie zawiera</u>';
 	else if (check.checked) napis.innerHTML = 'Kliknij raz na literę jeśli szukane słowo ją <u>zawiera</u>';
