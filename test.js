f495fd72961179076394f7cfd5bb579c2bab5fc1
mkaletaa <kaleta222@gmail.com>
2022-09-17 12:39:09 +0200

    icon added

--- test.js
+++
@@ -1,3 +0,0 @@
-// let text = "abace, abaci, abaka, abaką, abakę, abaki, abako, abaku, abata, abaty, abbie, abbom, abbów, abcug, abdul, abobr, abort, abrys, absta, abstą, abstę, absto, absty, absyd, abwer, acana, acani, acanu, acany";
-
-//console.log(text.replace(', ',','));
